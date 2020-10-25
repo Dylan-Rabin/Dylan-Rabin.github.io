20f07b8f8a71a0bc031e61cf07db85ebc5d4556b
Dylan Rabin <46030629+Dylan-Rabin@users.noreply.github.com>
2020-10-25 12:34:14 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ function toggle () {
   let menu = document.getElementById("side-menu");
 
   // toggle code
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  }
-  else {
+ if (menu.style.display === "none") {
     menu.style.display = "block";
   }
+  else{
+    menu.style.display = "none"; 
+  }
+} 
+  
   document.getElementById("ham-button").addEventListener("click",toggle);
-}
+
 
 
