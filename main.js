7512d6264d4232b5227782d0263ac09755e7c68a
Dylan Rabin <46030629+Dylan-Rabin@users.noreply.github.com>
2020-11-13 19:18:55 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -12,3 +12,10 @@ function toggle () {
   }
 } 
 
+//paste this code under the head tag or in a separate js file.
+	// Wait for window load
+	$(window).load(function() {
+		// Animate loader off screen
+    setTimeout(function(){ $(".se-pre-con").fadeOut("slow"); }, 500);
+	});
+
